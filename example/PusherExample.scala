e24202f4f9c918550392dbc905fc26a54c425e88
Vivan Kumar <vivangkumar@gmail.com>
2015-07-22 14:28:16 +0100

    Remove credentials

--- example/PusherExample.scala
+++ example/PusherExample.scala
@@ -2,7 +2,7 @@ import com.pusher._
 
 object PusherExample {
   def main (args: Array[String]) {
-    val config: PusherConfig = PusherConfig("124615", "224dd379428b25b8a1e4", "76ab1c5e5b47b22ad0b2")
+    val config: PusherConfig = PusherConfig("", "", "")
 
     val result = Pusher.trigger(config, List("test_channel"), "test_event", "test_event")
     result match {
@@ -11,4 +11,3 @@ object PusherExample {
     }
   }
 }
-
