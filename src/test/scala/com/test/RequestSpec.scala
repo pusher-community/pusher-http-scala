9476fb38e358f019cf640ef199136b562145fd85
Vivan Kumar <vivangkumar@gmail.com>
2015-08-02 17:45:08 +0100

    Add tests for sad cases and for ChannelsInfoResponse

--- src/test/scala/com/test/RequestSpec.scala
+++ src/test/scala/com/test/RequestSpec.scala
@@ -11,17 +11,65 @@ class RequestSpec extends FunSpec {
       assert(parseResponse[TriggerResponse]("{}") == Right(TriggerResponse()))
     }
 
-    it("should deserialise channel info JSON into ChannelInfoResponse") {
+    it("should deserialise channels info JSON into ChannelsInfoResponse with user_count") {
+      assert(parseResponse[ChannelsInfoResponse](
+        "{\"channels\":{\"presence-foobar\":{\"user_count\":42},\"presence-another\":{\"user_count\":123}}}")
+        ==
+        Right(
+          ChannelsInfoResponse(
+            Map("presence-foobar" -> ChannelDetails(Some(42)), "presence-another" -> ChannelDetails(Some(123)))
+          )
+        )
+      )
+    }
+
+    it("should deserialise channels info JSON into ChannelsInfoResponse without user_count") {
+      assert(parseResponse[ChannelsInfoResponse](
+        "{\"channels\":{\"presence-foobar\":{},\"presence-another\":{}}}")
+        ==
+        Right(
+          ChannelsInfoResponse(
+            Map("presence-foobar" -> ChannelDetails(None), "presence-another" -> ChannelDetails(None))
+          )
+        )
+      )
+    }
+
+    it("should deserialise channel info JSON into ChannelInfoResponse with all attributes") {
       assert(parseResponse[ChannelInfoResponse](
         "{\"occupied\":true,\"user_count\":42,\"subscription_count\":42}")
-        == Right(ChannelInfoResponse(occupied = true, Some(42), Some(42)))
+        ==
+        Right(
+          ChannelInfoResponse(
+            occupied = true,
+            Some(42),
+            Some(42)
+          )
+        )
+      )
+    }
+
+    it("should deserialise channel info JSON into ChannelInfoResponse with missing attributes") {
+      assert(parseResponse[ChannelInfoResponse](
+        "{\"occupied\":true}")
+        ==
+        Right(
+          ChannelInfoResponse(
+            occupied = true,
+            None,
+            None
+          )
+        )
       )
     }
 
     it("should deserialise users info JSON into UsersInfoResponse") {
       assert(parseResponse[UsersInfoResponse](
         "{\"users\":[{\"id\":\"1\"},{\"id\":\"2\"}]}")
-        == Right(UsersInfoResponse(List(UserDetails("1"), UserDetails("2"))))
+        ==
+        Right(UsersInfoResponse(
+          List(UserDetails("1"), UserDetails("2")))
+        )
       )
     }
 
@@ -29,5 +77,4 @@ class RequestSpec extends FunSpec {
       assert(parseResponse[TriggerResponse]("test") == Left(JSONParsingError("Failed to parse JSON: test")))
     }
   }
-
 }
